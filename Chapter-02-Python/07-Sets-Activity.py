232662378883d9fb4b0864d1bdb97dbc136c163c
waqvirk <virkwaqarahmad@gmail.com>
2025-08-11 16:26:42 +0200

    commiting Sets-Activity

---
+++ Chapter-02-Python/07-Sets-Activity.py
@@ -0,0 +1,22 @@
+print()
+#  1. Create a Set
+
+print("\n---\n")
+
+# 2. Check Membership
+
+print("\n---\n")
+
+# 3. Add and Update Items
+
+
+# 4. Remove Items
+
+
+# 5. Set Operations 
+
+
+# 6. In-place Set Operations
+
+
+# 7. Relational Methods
