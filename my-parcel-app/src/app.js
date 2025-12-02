5a740db77ea544886b2feb7c1d3d57889a256e2a
waqvirk <virkwaqarahmad@gmail.com>
2025-12-02 16:59:07 +0100

    added wbs coridnates

--- my-parcel-app/src/app.js
+++ my-parcel-app/src/app.js
@@ -1,1 +1,19 @@
+import * as Leaflet from 'leaflet';
+console.log(Leaflet);
 console.log('Hello world!');
+
+import Leaflet from 'leaflet'; // import everything from leaflet
+import 'leaflet/dist/leaflet.css'; // import leaflet css
+ 
+const WBS = [52.457131, 13.54007]; // WBS coordinates
+const map = Leaflet.map('map').setView(WBS, 13); // create a map object with a center and zoom level
+const markerIcon = Leaflet.icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  iconAnchor: [10, 20]
+}); // There was an issue with the default marker icon, so we create a new one
+Leaflet.marker(WBS, { icon: markerIcon }).addTo(map); // add a marker to the map at the WBS coordinates
+ 
+Leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://google.com" target="_blank">Google</a>'
+}).addTo(map); // add a tile layer to the map, the tiles are those images that make up the map
